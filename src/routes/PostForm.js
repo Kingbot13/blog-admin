020b102124f64cd85e68a9de4efc255c68e1eacd
Dylan King <dwkblue51@yahoo.com>
2022-10-25 15:42:17 -0500

    style PostForm

--- src/routes/PostForm.js
+++ src/routes/PostForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { posts } from "../posts";
 import { Form } from "react-router-dom";
+import styles from '../styles/PostForm.module.css';
 
 export async function action() {
   await posts.createPost();
@@ -8,7 +9,7 @@ export async function action() {
 
 export const PostForm = () => {
   return (
-    <Form method="post" id="post-form">
+    <Form method="post" id="post-form" className={styles.form}>
       <label htmlFor="title">
         Title:
         <input type="text" id="title" name="title" />
@@ -18,6 +19,7 @@ export const PostForm = () => {
         <input type="text" id="content" name="content" />
       </label>
       <label htmlFor="publish">
+        Publish: 
         <input type="checkbox" id="publish" name="publish" />
       </label>
       <button>Submit</button>
