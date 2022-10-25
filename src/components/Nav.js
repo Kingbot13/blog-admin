dbc31c679a4876938af8472679c1aa5a036e9ac1
Dylan King <dwkblue51@yahoo.com>
2022-10-25 08:40:10 -0500

    add styles to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styles from '../styles/Nav.module.css';
 
 export const Nav = () => {
   return (
-    <nav>
+    <nav className={styles.nav}>
       <div>
         <NavLink to="/create-post">Create Post</NavLink>
         <NavLink to="/sign-up">Sign Up</NavLink>
